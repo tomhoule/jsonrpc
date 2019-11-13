a1023273eb093df2362f4c51b683d2ae7a6289bc
Tom Houlé <tom@tomhoule.com>
2019-11-13 15:13:08 +0100

    Port stdio crate to tokio 0.2.0/std futures

--- stdio/examples/stdio.rs
+++ stdio/examples/stdio.rs
@@ -1,9 +1,10 @@
-use jsonrpc_stdio_server::jsonrpc_core::*;
+use jsonrpc_core::*;
 use jsonrpc_stdio_server::ServerBuilder;
 
-fn main() {
+#[tokio::main]
+async fn main() {
 	let mut io = IoHandler::default();
 	io.add_method("say_hello", |_params| Ok(Value::String("hello".to_owned())));
 
-	ServerBuilder::new(io).build();
+	ServerBuilder::new(io).build().await.unwrap();
 }
--- stdio/src/lib.rs
+++ stdio/src/lib.rs
@@ -3,31 +3,25 @@
 //! ```no_run
 //!
 //! use jsonrpc_stdio_server::ServerBuilder;
-//! use jsonrpc_stdio_server::jsonrpc_core::*;
+//! use jsonrpc_core::*;
 //!
-//! fn main() {
+//! #[tokio::main]
+//! async fn main() {
 //! 	let mut io = IoHandler::default();
 //! 	io.add_method("say_hello", |_params| {
 //! 		Ok(Value::String("hello".to_owned()))
 //! 	});
 //!
-//! 	ServerBuilder::new(io).build();
+//! 	ServerBuilder::new(io).build().await.unwrap();
 //! }
 //! ```
 
 #![deny(missing_docs)]
 
-use tokio;
-use tokio_stdin_stdout;
-#[macro_use]
-extern crate log;
-
-pub use jsonrpc_core;
-
+use futures03::{compat::*, stream::{StreamExt as _}, sink::SinkExt as _};
 use jsonrpc_core::IoHandler;
+use log::info;
 use std::sync::Arc;
-use tokio::prelude::{Future, Stream};
-use tokio_codec::{FramedRead, FramedWrite, LinesCodec};
 
 /// Stdio server builder
 pub struct ServerBuilder {
@@ -48,31 +42,33 @@ impl ServerBuilder {
 	/// Will block until EOF is read or until an error occurs.
 	/// The server reads from STDIN line-by-line, one request is taken
 	/// per line and each response is written to STDOUT on a new line.
-	pub fn build(&self) {
-		let stdin = tokio_stdin_stdout::stdin(0);
-		let stdout = tokio_stdin_stdout::stdout(0).make_sendable();
+	pub async fn build(&self) -> Result<(), Box<dyn std::error::Error>> {
+		use tokio::codec::{FramedRead, FramedWrite, LinesCodec};
 
-		let framed_stdin = FramedRead::new(stdin, LinesCodec::new());
-		let framed_stdout = FramedWrite::new(stdout, LinesCodec::new());
+		let stdin = tokio::io::stdin();
+		let stdout = tokio::io::stdout();
 
-		let handler = self.handler.clone();
-		let future = framed_stdin
-			.and_then(move |line| process(&handler, line).map_err(|_| unreachable!()))
-			.forward(framed_stdout)
-			.map(|_| ())
-			.map_err(|e| panic!("{:?}", e));
+		let mut framed_stdin = FramedRead::new(stdin, LinesCodec::new());
+		let mut framed_stdout = FramedWrite::new(stdout, LinesCodec::new());
 
-		tokio::run(future);
+		while let Some(line) = framed_stdin.next().await.transpose()? {
+			let response: String = process_one_request(&self.handler, line).await.unwrap();
+			framed_stdout.send(response).await?;
+		}
+
+		Ok(())
 	}
 }
 
 /// Process a request asynchronously
-fn process(io: &Arc<IoHandler>, input: String) -> impl Future<Item = String, Error = ()> + Send {
-	io.handle_request(&input).map(move |result| match result {
-		Some(res) => res,
+async fn process_one_request(io: &Arc<IoHandler>, input: String) -> Result<String, ()> {
+	let response = io.handle_request(&input).compat().await?;
+
+	match response {
+		Some(res) => Ok(res),
 		None => {
 			info!("JSON RPC request produced no response: {:?}", input);
-			String::from("")
+			Ok(String::from(""))
 		}
-	})
+	}
 }
